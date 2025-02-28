bb3b8d2754cfd495a3e744924f61b63cead820c1
Shashvat <shashvat@foureightseven.studio>
2025-02-28 14:22:33 +0530

    typgraphy fix on mobile and theme saved to local storage

--- assets/js/random_color.js
+++ assets/js/random_color.js
@@ -1,4 +1,20 @@
 window.accentColor = {
+    gen: () => {
+        window.accentColor = {
+            ...window.accentColor,
+            hue: Math.floor(Math.random() * 360),
+            saturation: 100,
+        };
+        window.accentColor.lightness = window.accentColor.get_lightness();
+    },
+    init: () => {
+        if (localStorage) {
+            const theme = localStorage.getItem("theme");
+            if (theme == "light") {
+                window.accentColor.toggle_theme();
+            }
+        }
+    },
     set: () => {
         const { hue, saturation, lightness } = window.accentColor;
         const root = document.documentElement;
@@ -7,18 +23,26 @@ window.accentColor = {
         body.style.setProperty("--accent", accent);
         body.style.setProperty("--accent-foreground", accent);
     },
-    gen: () => {
-        window.accentColor = {
-            ...window.accentColor,
-            hue: Math.floor(Math.random() * 360),
-            saturation: 100,
-        };
-        window.accentColor.lightness = window.accentColor.get_lightness()
+    toggle_theme: () => {
+        document.querySelector("body").classList.toggle("dark");
+        window.accentColor.store();
+
+        window.accentColor.lightness = window.accentColor.get_lightness();
+        window.accentColor.set();
     },
     get_lightness: () => {
-        return document.querySelector("body").classList.contains("dark") ? 45 : 14;
+        return window.accentColor.theme() == "dark" ? 45 : 14;
+    },
+    theme: () => {
+        return document.querySelector("body").classList.contains("dark")
+            ? "dark"
+            : "light";
+    },
+    store: () => {
+        localStorage.setItem("theme", window.accentColor.theme());
     },
 };
 
 window.accentColor.gen();
+window.accentColor.init();
 window.accentColor.set();
--- content/about/_index.md
+++ content/about/_index.md
@@ -3,7 +3,7 @@ title: "About"
 ---
 
 Hello there! I used to be an aspiring Physicist but now I
-develop software for a living(and for fun!). Occasionally, I
+develop software for a living(and for fun!). I
 write about things that capture my attention or things
 others might find interesting. Take some time out to check out my posts,
-other works or contact me at hello@shashvat.dev.
+other works or contact me at <span class=" hyphens-none">shashvat817@gmail.com</span>.
